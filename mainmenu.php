5806e2803554918bda38caf64580c2cdd8e97eb5
judeharding <jharding@genesisgroupusa.com>
2017-07-26 15:08:32 -0400

    strapping main page

--- mainmenu.php
+++ mainmenu.php
@@ -1,19 +1,20 @@
+<?php
+    require("includes/db.php");
+    require("header.php");
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Gift Registry</title>
-        <!-- <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script> -->
         <?php
             require ("header.php");
         ?>
     </head>
     <body>
         <h1>Welcome to THE Gift Registry</h1>
-        <a href="account_list.php">Account List</a><br>
-        <a href="account.php?action=insert">Add Account</a>
+        <a  class="btn btn-default" href="account_list.php">Account List</a><br>
+        <a  class="btn btn-default" href="account.php?action=insert">Add Account</a>
 
     </body>
 </html>
